c3a59d337de56c6b04282f68bbf5d66b92fe650a
君赏 <josercc@users.noreply.github.com>
2019-03-02 10:29:29 +0800

    Create Package.swift

---
+++ SwiftPerfect/Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "{{name}}",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        {{#dependencies}}
+        .package(url: "{{url}}", {{version}}})
+        {{/dependencies}}
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "{{name}}",
+            dependencies: [{{dependenciesTargets}}}]),
+    ]
+)
